22b6d5606d255890a92eec1d676428461ecb0242
fish-inu <blasthotcorn@protonmail.com>
2020-11-25 21:36:10 +0800

    updated docs-zh pot

--- docs-zh/convert2po.sh
+++ docs-zh/convert2po.sh
@@ -6,7 +6,7 @@ for path in ${arr[@]}; do
     echo "正在转换 ${path}"
     docs_removed=${path##docs/}
     ext2pot=${docs_removed%.md}.pot
-    $HOME/Downloads/po4a-0.61/po4a-gettextize --format text --master $path --master-charset "UTF-8" --po docs-zh/${ext2pot}
+    $HOME/Downloads/po4a-0.61/po4a-gettextize --format text -o markdown --master $path --master-charset "UTF-8" --po docs-zh/${ext2pot}
     #cp docs-zh/${ext2pot} docs-zh/${ext2pot%.pot}.zh_Hans.po
     echo "---OK---"
 done    
